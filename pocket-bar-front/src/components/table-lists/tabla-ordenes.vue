e8f1bf25446e60922aae40e59852f85df9ea943a
Piaczio <trotroyu@gmail.com>
2022-02-12 12:49:03 -0700

    feat: ordenes

--- pocket-bar-front/src/components/table-lists/tabla-ordenes.vue
+++ pocket-bar-front/src/components/table-lists/tabla-ordenes.vue
@@ -1,130 +1,135 @@
 <template>
-  <div id="app">
+  <div class="tabla" id="app">
+    <v-row>
+      <v-col cols="12" sm="6" md="4">
+        <v-text-field
+          v-model="search"
+          label="Buscar actividad"
+          class="mx-4"
+          id="onsearch"
+        ></v-text-field>
+      </v-col>
+    </v-row>
     <v-app id="inspire">
-      <v-card>
-        <v-card-title>
-          <v-text-field
-            v-model="search"
-            append-icon="mdi-magnify"
-            label="Search"
-            single-line
-            hide-details
-          ></v-text-field>
-        </v-card-title>
-        <v-data-table
-          :headers="headers"
-          :items="desserts"
-          :search="search"
-        ></v-data-table>
-      </v-card>
+      <v-progress-linear
+        height="6"
+        indeterminate
+        color="cyan"
+        :active="cargando"
+      ></v-progress-linear>
+      <v-data-table
+        id="tabla"
+        :headers="headers"
+        show-expand
+        :expanded.sync="expanded"
+        :items="activitylogArray"
+        sort-by="cantidad_articulo"
+        class="elevation-1"
+        :search="search"
+        :custom-filter="filterOnlyCapsText.toUpperCase"
+      >
+        <template v-slot:top>
+          <v-toolbar flat>
+            <v-toolbar-title>Tabla historial</v-toolbar-title>
+            <v-divider class="mx-4" inset vertical></v-divider>
+            <v-spacer></v-spacer>
+          </v-toolbar>
+        </template>
+        <template v-slot:no-data>
+          <span>Datos no disponibles.</span>
+        </template>
+        <template v-slot:expanded-item="{ headers, item }">
+          <td :colspan="headers.length">
+            {{ item.properties }}
+          </td>
+        </template>
+      </v-data-table>
     </v-app>
   </div>
 </template>
 
 <script>
+import { getActivitylog } from "@/api/activitylog.js";
+
 export default {
-  data() {
-    return {
-      search: "",
-      headers: [
-        {
-          text: "Dessert (100g serving)",
-          align: "start",
-          filterable: false,
-          value: "name",
-        },
-        { text: "Calories", value: "calories" },
-        { text: "Fat (g)", value: "fat" },
-        { text: "Carbs (g)", value: "carbs" },
-        { text: "Protein (g)", value: "protein" },
-        { text: "Iron (%)", value: "iron" },
-      ],
-      desserts: [
-        {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: "1%",
-        },
-        {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: "1%",
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: "7%",
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: "8%",
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: "16%",
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: "0%",
-        },
-        {
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: "2%",
-        },
-        {
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: "45%",
-        },
-        {
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: "22%",
-        },
-        {
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: "6%",
-        },
-      ],
-    };
+  name: "tabla-activitylog",
+  data: () => ({
+    search: "",
+    cargando: true,
+    expanded: [],
+    headers: [
+      {
+        text: "Actividad",
+        align: "start",
+        sortable: false,
+        value: "accion",
+      },
+      {
+        text: "Responsable",
+        align: "start",
+        sortable: false,
+        value: "causername",
+      },
+      {
+        text: "Creado en",
+        align: "start",
+        sortable: false,
+        value: "createdat",
+      },
+      {
+        text: "Actualizado en",
+        align: "start",
+        sortable: false,
+        value: "updatedat",
+      },
+      { text: "Descripción", align: "start", value: "data-table-expand" },
+    ],
+
+    activitylogArray: [],
+  }),
+  mounted() {
+    this.onFocus();
+    // window.Echo.channel("activitylog").listen("activitylogCreated", (e) => {
+    //   this.activitylogArray = e.activitylog;
+    // });
+    getActivitylog(this.activitylogArray)
+      .then((response) => {
+        if (response.stats === 200) {
+          this.cargando = false;
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+        this.cargando = true;
+      });
+  },
+  methods: {
+    onFocus() {
+      let stext = document.getElementById("onsearch");
+      stext;
+      stext = addEventListener("keydown", (e) => {
+        if (e.altKey) {
+          document.getElementById("onsearch").focus();
+        }
+      });
+    },
+    filterOnlyCapsText(value, search) {
+      return (
+        value != null &&
+        search != null &&
+        typeof value === "string" &&
+        value.toString().toLocaleUpperCase().indexOf(search) !== -1
+      );
+    },
   },
 };
 </script>
 
-<style>
+<style scoped>
+#tabla {
+  width: 100%;
+}
+.tabla {
+  width: 100%;
+}
 </style>

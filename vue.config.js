63b683cac4f52367271fa4b32dbd920cf7c7f2dd
Piaczio <trotroyu@gmail.com>
2022-05-02 09:37:49 -0600

    feat: Configs indent

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,8 @@ module.exports = {
  // devServer Options don't belong into `configureWebpack`
  devServer: {
   host: '0.0.0.0',
+  webSocketURL: 'ws://0.0.0.0:6001',
+
   hot: true,
   disableHostCheck: true,
  },

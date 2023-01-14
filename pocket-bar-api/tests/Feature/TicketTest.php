92d930887cce4826495c644d6b4826fccec19b40
farid <faridglez17@gmail.com>
2023-01-13 23:27:20 -0700

    add: log in test

--- pocket-bar-api/tests/Feature/TicketTest.php
+++ pocket-bar-api/tests/Feature/TicketTest.php
@@ -3,12 +3,14 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class TicketTest extends TestCase
 {
     /**
-     * A basic test example.
+     * A basic feature test example.
      *
      * @return void
      */
@@ -16,5 +18,6 @@ class ExampleTest extends TestCase
     {
         $response = $this->post('/api/login', ["name"=>"admin", "password"=>"12345678"]);
         $response->assertStatus(200);
+
     }
 }

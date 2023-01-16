96c6ffe78f9b5c2af7d8dd158803a88d24c7b973
Lumexio <trotroyu@gmail.com>
2023-01-15 22:59:23 -0700

    fix: Butons select tip

--- pocket-bar-front/src/pwa-components/forms/ticket-viewer.vue
+++ pocket-bar-front/src/pwa-components/forms/ticket-viewer.vue
@@ -30,22 +30,22 @@
 						Titular
 						<h1>{{ ticket.titular }}</h1>
 					</v-col>
-					<v-spacer></v-spacer>
-					<v-col cols="2">
+
+					<v-col>
 						Estatus
 						<h3>{{ ticket.status }}</h3>
 					</v-col>
-					<v-col cols="1">
+					<v-col>
 						Mesa
 						<h3>{{ ticket.nombre_mesa }}</h3>
 					</v-col>
-					<v-col cols="2">
+					<v-col>
 						Fecha
 						<h3>{{ ticket.fecha }}</h3>
 					</v-col>
 				</v-row>
 				<v-row>
-					<v-col cols="5"
+					<!-- <v-col cols="5"
 						><v-select
 							v-model="selectip"
 							:items="itemstip"
@@ -54,32 +54,43 @@
 							outlined
 							class="ma-2"
 						></v-select
-					></v-col>
-					<v-col>
-						<v-btn class="ma-3" large><v-icon>mdi-cash</v-icon></v-btn>
-					</v-col></v-row
-				>
+					></v-col> -->
+
+					<template v-for="(item, index) in itemstip" :item="item">
+						<v-col class="d-flex justify-center" :key="index">
+							<v-btn
+								dark
+								color="primary"
+								@click="toTip(item.value)"
+								large
+								rounded
+								depressed
+								>{{ item.name }}</v-btn
+							>
+						</v-col>
+					</template>
+				</v-row>
 				<v-simple-table dense calculate-widths>
 					<template v-slot:default>
 						<thead>
 							<tr>
-								<th class="text-left">Nombre</th>
-								<th class="text-left">Cantidad</th>
-								<th class="text-left">Precio</th>
-
-								<th class="text-left">Descuento</th>
-								<th class="text-left">Total</th>
+								<th class="text-center pa-0">Nombre</th>
+								<th class="text-center pa-0">Cantidad</th>
+								<th class="text-center pa-0">Precio</th>
+								<th class="text-center pa-0">Descuento</th>
+								<th class="text-center pa-0">Total</th>
 							</tr>
 						</thead>
 						<tbody>
 							<tr v-for="producto in ticket.productos" :key="producto.id">
-								<td class="text-left">{{ producto.nombre }}</td>
-								<td class="text-left">{{ producto.cantidad }}</td>
-								<td class="text-left">{{ producto.precio }}</td>
-								<td class="text-left">${{ producto.descuento }}</td>
-								<td class="text-left">${{ producto.total }}</td>
+								<td class="text-center">{{ producto.nombre }}</td>
+								<td class="text-center">{{ producto.cantidad }}</td>
+								<td class="text-center">{{ producto.precio }}</td>
+								<td class="text-center">${{ producto.descuento }}</td>
+								<td class="text-center">${{ producto.total }}</td>
 							</tr>
 						</tbody>
+						<v-row class="ma-3"> Propina: {{ selectip }}%</v-row>
 					</template>
 				</v-simple-table>
 			</v-card-text>
@@ -94,9 +105,20 @@ export default {
 		ticket: { default: null },
 	} /*data de llegado de componente padre creacion*/,
 	data() {
-		return { itemstip: [5, 10, 15, 20], selectip: 0 };
+		return {
+			itemstip: [
+				{ name: "5%", value: 5 },
+				{ name: "10%", value: 10 },
+				{ name: "15%", value: 15 },
+				{ name: "20%", value: 20 },
+			],
+			selectip: 0,
+		};
 	},
 	methods: {
+		toTip(tip) {
+			this.selectip = tip;
+		},
 		swipe(direction) {
 			if (direction === "Down") {
 				this.$emit("update:dialogticketviewer", false);

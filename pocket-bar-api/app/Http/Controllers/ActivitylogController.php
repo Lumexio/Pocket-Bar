a5704c384c49bfda88b80aba30d4c571c807a670
farid <faridglez17@gmail.com>
2023-06-18 10:42:22 -0600

    feat: activity log index

--- pocket-bar-api/app/Http/Controllers/ActivitylogController.php
+++ pocket-bar-api/app/Http/Controllers/ActivitylogController.php
@@ -11,7 +11,29 @@ class ActivitylogController extends Controller
 {
     public function index(): JsonResponse
     {
-        $dat = DB::table("activity_log")->join('users', 'activity_log.causer_id', '=', 'users.id')->select("activity_log.description", "users.name", "activity_log.properties",  "activity_log.created_at", "activity_log.updated_at")->get();
+        $dat = DB::table("activity_log")->join('users', 'activity_log.causer_id', '=', 'users.id')->select("activity_log.description", "users.name", "activity_log.subject_type", "activity_log.properties",  "activity_log.created_at", "activity_log.updated_at")->get();
+        $dat = $dat->map(function ($item) {
+            $item->properties = json_decode($item->properties, true);
+            $subject = str_replace("App\Models\\", "", $item->subject_type);
+            $item->description = match ($item->description) {
+                "created" => "Creación",
+                "updated" => "Actualización",
+                "deleted" => "Eliminación",
+                default => "Otro"
+            };
+            $item->subject_type = match ($subject) {
+                "User" => "Usuario",
+                "Articulo" => "Artículo",
+                "Rol" => "Rol",
+                "Categoria" => "Categoría",
+                "Proveedor" => "Proveedor",
+                "Tipo" => "Tipo de Artículo",
+                "Ticket" => "Ticket",
+                default => "Otro"
+            };
+            $item->description = $item->description . " de " . $item->subject_type;
+            return $item;
+        });
         return response()->json(
             [
                 "status" => 200,

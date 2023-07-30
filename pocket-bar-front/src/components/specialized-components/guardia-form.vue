9389ee0f9402ff87ebf7c70ea74817b483aa08e7
Piaczio <trotroyu@gmail.com>
2023-07-30 15:39:45 -0600

    fix: Guardia screens

---
+++ pocket-bar-front/src/components/specialized-components/guardia-form.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card>
+   <v-card-title>Cobrar entrada</v-card-title>
+   <v-card-text>
+    <v-text-field
+						label="Cover"
+            prepend-icon="mdi-cash"
+						required
+					></v-text-field>
+   </v-card-text>
+   <v-card-actions><v-btn>Cancelar</v-btn><v-btn>Aceptar</v-btn></v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+name:"guardia-form"
+}
+</script>
+
+<style>
+
+</style>

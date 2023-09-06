6e3c3893647d6a755fc16eaca0f5237ef1132971
Piaczio <trotroyu@gmail.com>
2023-09-06 12:17:51 -0600

    fix: Changes on broadcast

--- pocket-bar-api/app/Events/BarraEvents.php
+++ pocket-bar-api/app/Events/BarraEvents.php
@@ -5,11 +5,11 @@ namespace App\Events;
 use App\Models\TicketDetail;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class BarraEvents implements ShouldBroadcast
+class BarraEvents implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 

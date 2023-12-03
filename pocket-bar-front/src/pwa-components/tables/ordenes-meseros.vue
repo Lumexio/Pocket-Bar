d4dfd24da1423810a5e550be5d553340e3c24bef
Francisco.salazar <francisco.salazar@fiexact.com>
2023-12-03 16:37:40 -0700

    fix: Mesero ui changes

--- pocket-bar-front/src/pwa-components/tables/ordenes-meseros.vue
+++ pocket-bar-front/src/pwa-components/tables/ordenes-meseros.vue
@@ -1,60 +1,39 @@
 <template >
 	<v-expansion-panels :dark="darkonchange" popout>
-		<v-expansion-panel class="sizes" v-for="item in ticketsPWAArray" :key="item.id" v-show="item.status == hasstatus">
-			<v-expansion-panel-header>
-				<div class="grid-show">
-					<span style="grid-column: span 3;">
-						<span style="font-size: 18px">
-							<b>Titular: </b>{{ item.titular }}</span>
-						<br />
-						<span style="font-size: 18px"><b>Mesa: </b>{{ item.nombre_mesa }}</span>
-						<br />
-						<span style="font-size: 18px"> <b>Fecha: </b>{{ item.fecha }}</span>
-					</span>
-					<v-btn class="button-list" :dark="darkonchange" color="cyan darken-3"
-						@click.prevent="(dialogticketviewer = true), (onticket = item)"><v-icon>mdi-eye-circle</v-icon>
-					</v-btn>
-					<v-btn class="button-list" :dark="darkonchange" color="#1E88E5" v-if="item.status != 'Cerrado'"
-						@click.prevent="(dialogaddproduct = true), (ticket_id = item.id)"><v-icon>mdi-plus</v-icon>
-					</v-btn>
+		<v-progress-linear color="deep-purple accent-4" indeterminate rounded height="6"
+			:active="carga"></v-progress-linear>
+		<v-card style="inline-size:100%;" v-for="item in ticketsPWAArray" :key="item.id" v-show="item.status == hasstatus">
+			<v-card-title class="text-start">
+				<span>
+					<b>Titular: </b>{{ item.titular }}
+					<br />
+					<b>Mesa: </b>{{ item.nombre_mesa }}
+					<br />
+					{{ item.fecha }}
+				</span>
+			</v-card-title>
+			<v-card-text class="text-start">
+				<div v-if="item.tip > 0" class="d-flex flex-row justify-space-between align-center">
+					<span> Propina:</span>
+					<v-divider></v-divider>
+					<b>${{ item.tip }}</b>
 				</div>
-			</v-expansion-panel-header>
-			<v-expansion-panel-content class="expansion-panel">
-				<v-simple-table dense calculate-widths>
-					<template v-slot:default>
-						<thead>
-							<tr>
-								<th class="text-left">Nombre</th>
-								<th class="text-left">Cantidad</th>
-								<th class="text-left">Precio</th>
-								<th class="text-left">Total</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr v-for="producto in item.productos" :key="producto.id">
-								<td class="text-left">{{ producto.nombre }}</td>
-								<td class="text-left">{{ producto.cantidad }}</td>
-								<td class="text-left">${{ producto.precio }}</td>
-								<td class="text-left">${{ producto.total }}</td>
-							</tr>
-						</tbody>
-					</template>
-				</v-simple-table>
+				<br />
+				<div class="d-flex flex-row justify-space-between align-center">
+					<span> Subtotal:</span><v-divider></v-divider> <b> ${{ item.total }}</b>
+				</div>
+			</v-card-text>
+			<v-card-actions>
 
-				<v-col class="pa-4">
-					<div v-if="item.tip > 0" class="d-flex flex-row justify-space-between align-center">
-						<span> Propina:</span>
-						<v-divider></v-divider>
-						<b>{{ item.tip }}% </b>
-					</div>
-					<div class="d-flex flex-row justify-space-between align-center">
-						<span> Subtotal:</span><v-divider></v-divider> <b> ${{ item.total }}</b>
-					</div>
-				</v-col>
-
-			</v-expansion-panel-content>
-			<ticketViewer :dialogticketviewer.sync="dialogticketviewer" :ticket="onticket" />
-		</v-expansion-panel>
+				<v-btn :dark="darkonchange" color="cyan darken-3"
+					@click.prevent="(dialogticketviewer = true), (onticket = item)"><v-icon>mdi-eye-circle</v-icon>
+				</v-btn>
+				<v-btn :dark="darkonchange" color="#1E88E5" v-if="item.status != 'Cerrado'"
+					@click.prevent="(dialogaddproduct = true), (ticket_id = item.id)"><v-icon>mdi-plus</v-icon>
+				</v-btn>
+			</v-card-actions>
+		</v-card>
+		<ticketViewer :dialogticketviewer.sync="dialogticketviewer" :ticket="onticket" />
 		<addProducts :dialogaddproduct.sync="dialogaddproduct" :ticket_id="ticket_id" />
 	</v-expansion-panels>
 </template>

58d469ec71e327219662dbaa246b3d4bd8521dfc
Lumexio <trotroyu@gmail.com>
2024-04-28 13:45:12 -0600

    fix: command stripe inserion plan

--- pocket-bar-api/app/Console/Commands/CreateProductPlans.php
+++ pocket-bar-api/app/Console/Commands/CreateProductPlans.php
@@ -42,98 +42,95 @@ class CreateProductPlans extends Command
     {
         // **Los ultimos dos digitos de la propierdad amount son los centavos
         Stripe::setApiKey(env('STRIPE_SECRET'));
-        // $product = \Stripe\Product::retrieve([
-        //     'id' => 'prod_PSgrmpZCPmOTIi',
-
-        // ]);
-        // if (empty($product)) {
-
-        // }
-        // echo $product->name;
-        $product = \Stripe\Product::create([
-            'name' => 'Beer Tier',
-        ]);
-        $benefits = [
-            'Gestion de inventarios',
-            'Toma de ordenes',
-            'Cobro de cover',
-            'Comandas digitales',
-        ];
-        // crear el plan mensual y anual de 10 y 100 dolares respectivamente
-        $stripePlan = \Stripe\Plan::create([
-            'product' => $product->id,
-            'nickname' => 'Beer Monthly',
-            'interval' => 'month',
-            'currency' => 'usd',
-            'metadata' => $benefits,
-            'amount' => 1000,
-        ]);
-        // insertar los planes en la base de datos
-        $plan = new \App\Models\Plan();
-        $plan->name = $stripePlan->nickname;
-        $plan->stripe_id = $stripePlan->id;
-        $plan->interval = $stripePlan->interval;
-        $plan->currency = $stripePlan->currency;
-        $plan->amount = $stripePlan->amount;
-        $plan->benefits =  \implode(',', $benefits);
-        $plan->save();
-        $stripePlan = \Stripe\Plan::create([
-            'product' => $product->id,
-            'nickname' => 'Beer Annual',
-            'interval' => 'year',
-            'currency' => 'usd',
-            'metadata' => $benefits,
-            'amount' => 10000,
-        ]);
-        // insertar los planes en la base de datos
-        $plan = new \App\Models\Plan();
-        $plan->name = $stripePlan->nickname;
-        $plan->stripe_id = $stripePlan->id;
-        $plan->interval = $stripePlan->interval;
-        $plan->currency = $stripePlan->currency;
-        $plan->amount = $stripePlan->amount;
-        $plan->benefits = \implode(',', $benefits);
-        $plan->save();
-        $product = \Stripe\Product::create([
-            'name' => 'Mojito Tier',
-        ]);
-        $benefits = [
-            'Gestion de inventarios', 'Toma de ordenes', 'Cobro de cover'
-        ];
-        // crear el plan mensual y anual de 10 y 100 dolares respectivamente
-        $stripePlan = \Stripe\Plan::create([
-            'product' => $product->id,
-            'nickname' => 'Mojito Monthly',
-            'interval' => 'month',
-            'currency' => 'usd',
-            'metadata' => $benefits,
-            'amount' => 2000,
-        ]);
-        // insertar los planes en la base de datos
-        $plan = new \App\Models\Plan();
-        $plan->name = $stripePlan->nickname;
-        $plan->stripe_id = $stripePlan->id;
-        $plan->interval = $stripePlan->interval;
-        $plan->currency = $stripePlan->currency;
-        $plan->amount = $stripePlan->amount;
-        $plan->benefits =  \implode(',', $benefits);
-        $plan->save();
-        $stripePlan = \Stripe\Plan::create([
-            'product' => $product->id,
-            'nickname' => 'Mojito Annual',
-            'interval' => 'year',
-            'currency' => 'usd',
-            'metadata' => $benefits,
-            'amount' => 20000,
-        ]);
-        // insertar los planes en la base de datos
-        $plan = new \App\Models\Plan();
-        $plan->name = $stripePlan->nickname;
-        $plan->stripe_id = $stripePlan->id;
-        $plan->interval = $stripePlan->interval;
-        $plan->currency = $stripePlan->currency;
-        $plan->amount = $stripePlan->amount;
-        $plan->benefits =  \implode(',', $benefits);
-        $plan->save();
+        $product = \Stripe\Product::all();
+        if (empty($product->data)) {
+            $product = \Stripe\Product::create([
+                'name' => 'Beer Tier',
+            ]);
+            $benefits = [
+                'Gestion de inventarios',
+                'Toma de ordenes',
+                'Cobro de cover',
+                'Comandas digitales',
+            ];
+            // crear el plan mensual y anual de 10 y 100 dolares respectivamente
+            $stripePlan = \Stripe\Plan::create([
+                'product' => $product->id,
+                'nickname' => 'Beer Monthly',
+                'interval' => 'month',
+                'currency' => 'usd',
+                'metadata' => $benefits,
+                'amount' => 1000,
+            ]);
+            // insertar los planes en la base de datos
+            $plan = new \App\Models\Plan();
+            $plan->name = $stripePlan->nickname;
+            $plan->stripe_id = $stripePlan->id;
+            $plan->interval = $stripePlan->interval;
+            $plan->currency = $stripePlan->currency;
+            $plan->amount = $stripePlan->amount;
+            $plan->benefits =  \implode(',', $benefits);
+            $plan->save();
+            $stripePlan = \Stripe\Plan::create([
+                'product' => $product->id,
+                'nickname' => 'Beer Annual',
+                'interval' => 'year',
+                'currency' => 'usd',
+                'metadata' => $benefits,
+                'amount' => 10000,
+            ]);
+            // insertar los planes en la base de datos
+            $plan = new \App\Models\Plan();
+            $plan->name = $stripePlan->nickname;
+            $plan->stripe_id = $stripePlan->id;
+            $plan->interval = $stripePlan->interval;
+            $plan->currency = $stripePlan->currency;
+            $plan->amount = $stripePlan->amount;
+            $plan->benefits = \implode(',', $benefits);
+            $plan->save();
+            $product = \Stripe\Product::create([
+                'name' => 'Mojito Tier',
+            ]);
+            $benefits = [
+                'Gestion de inventarios', 'Toma de ordenes', 'Cobro de cover'
+            ];
+            // crear el plan mensual y anual de 10 y 100 dolares respectivamente
+            $stripePlan = \Stripe\Plan::create([
+                'product' => $product->id,
+                'nickname' => 'Mojito Monthly',
+                'interval' => 'month',
+                'currency' => 'usd',
+                'metadata' => $benefits,
+                'amount' => 2000,
+            ]);
+            // insertar los planes en la base de datos
+            $plan = new \App\Models\Plan();
+            $plan->name = $stripePlan->nickname;
+            $plan->stripe_id = $stripePlan->id;
+            $plan->interval = $stripePlan->interval;
+            $plan->currency = $stripePlan->currency;
+            $plan->amount = $stripePlan->amount;
+            $plan->benefits =  \implode(',', $benefits);
+            $plan->save();
+            $stripePlan = \Stripe\Plan::create([
+                'product' => $product->id,
+                'nickname' => 'Mojito Annual',
+                'interval' => 'year',
+                'currency' => 'usd',
+                'metadata' => $benefits,
+                'amount' => 20000,
+            ]);
+            // insertar los planes en la base de datos
+            $plan = new \App\Models\Plan();
+            $plan->name = $stripePlan->nickname;
+            $plan->stripe_id = $stripePlan->id;
+            $plan->interval = $stripePlan->interval;
+            $plan->currency = $stripePlan->currency;
+            $plan->amount = $stripePlan->amount;
+            $plan->benefits =  \implode(',', $benefits);
+            $plan->save();
+        } else {
+            echo 'Products already created on stripe';
+        }
     }
 }
